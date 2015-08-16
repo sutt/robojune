e8231b4a1316a008c54ea983058a6edf0cb63d06
sutt <wsutton17@gmail.com>
2015-08-15 19:01:44 -0700

    routing

--- hello.py
+++ hello.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
 	return 'Hello World!'
+	
+@app.route('/takepic/')
+def takepic():
+	return 'taking pic'
 
 if __name__== "__main__":
 	app.run(host='0.0.0.0')
