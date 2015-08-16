5c73fdd8493f74fde304c6dae38603f1d864ace4
sutt <wsutton17@gmail.com>
2015-08-15 20:30:54 -0700

    data on smoothie req

--- hello.py
+++ hello.py
@@ -46,7 +46,7 @@ def smoothie(data):
 	print "down to ret"
 	#ret = [out1.join(" | ") ,out2.join(" | ")].join("\n")
 	print out1
-	ret = out1.join(" | ")
+	ret = " | ".join(out1)
 	return ret 
 	  
 if __name__== "__main__":
