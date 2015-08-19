e3b0b0e7fd9c7b23c7a21084e387c60d9be87002
sutt <wsutton17@gmail.com>
2015-08-19 22:43:31 +0000

    stashing

--- spin/rotate.py
+++ spin/rotate.py
@@ -1,7 +1,18 @@
-import time
+import time, sys
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD)
+ss = .025
+
+if len(sys.argv) > 1:
+	steps = int(sys.argv[1])
+else:
+	steps = 50
+
+if len(sys.argv) > 2:
+	dirinp = GPIO.LOW
+else:
+	dirinp = GPIO.LOW
 
 def rotate():
 	sleep,dir,step = 7,3,5
@@ -10,14 +21,18 @@ def rotate():
 	GPIO.setup(step, GPIO.OUT)
 	
 	GPIO.output(sleep,GPIO.LOW)
-	time.sleep(ss)
+	time.sleep(1.5)
 	GPIO.output(sleep,GPIO.HIGH)
 	
-	
-	for i in range(100):
+	time.sleep(.01)
+	GPIO.output(dir,dirinp)
+	time.sleep(.01)
+
+	for i in range(steps):
 		GPIO.output(step,GPIO.LOW)
+		print i
 		time.sleep(ss)
-		GPIO.output(step,GPIO.HOGH)
+		GPIO.output(step,GPIO.HIGH)
 		time.sleep(ss)
 
 rotate()
